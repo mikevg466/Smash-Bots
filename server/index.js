7ed357f58ab1a26a577b0908ad1453eb9c204696
mikevg466 <mikevg466@gmail.com>
2017-06-23 14:26:20 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -56,26 +56,38 @@ const listenUp = () => {
 };
 
 
-// server rooms does is always empty
+// server rooms is always empty
 // server.sockets.adapter.rooms has rooms but also has clientID's with no way to distinguish
 // client.rooms has rooms but also it's own id also with no real way to distinguish besides
 //      looking to see if roomId === client.id
 
-// going to try puting lobby- before room uuid to be able to filter
-const makeSocketServer = () => {
-  var clients = {};
+// example: 
+// server.sockets.adapter.rooms = {
+//    123812903: 123812903,
+//    654654742: 654654742,
+//    457456345: 457456345,
+//    room-12412432: room-12412432,
+//    room-43534513: room-43534513
+// }
 
+// example:
+// client.rooms = {
+//    457456345: 457456345,
+//    room-12412432: room-12412432,
+//}
+
+const makeSocketServer = () => {
   // create helper function to find all rooms on server adapter that starts with lobby-
   //    this is used for every update call so helper is DRY
   const findRoomsOnServer = () => {
     let rooms = server.sockets.adapter.rooms ? Object.keys(server.sockets.adapter.rooms) : [];
-    rooms = rooms.filter(room => room.length > 6 && room.slice(0, 6) === 'lobby-');
+    rooms = rooms.filter(room => room.length > 5 && room.slice(0, 5) === 'room-');
     return rooms
   }
 
   const findRoomForClient = client => {
     const rooms = client.rooms ? Object.keys(client.rooms) : [];
-    const room = rooms.find(room => room.length > 6 && room.slice(0, 6) === 'lobby-');
+    const room = rooms.find(room => room.length > 5 && room.slice(0, 5) === 'room-');
     return room;
   }
 
@@ -95,7 +107,7 @@ const makeSocketServer = () => {
     // using join for both join and host
     //   use default roomId so that if they are not joining an existing room
     //    they are creating a new room
-    client.on('join', (roomID = 'lobby-' + uuid.v4()) => {
+    client.on('join', (roomID = 'room-' + uuid.v4()) => {
       // join or create room
       client.join(roomID);
       server.sockets.emit('update', findRoomsOnServer());
