5c685268ab5c3328ebb39c42bc02737e2a904090
mikevg466 <mikevg466@gmail.com>
2017-06-26 17:44:51 -0400

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,7 +2,9 @@ const db = require('../db');
 const User = require('./user');
 const Item = require('./item');
 
- User.hasMany(Item)
+User.hasMany(Item, { through: 'purchased_items' });
+User.hasOne(Item, { as: 'weapon' });
+User.hasOne(Item, { as: 'armor' });
 
 
 module.exports = {
