8bd7ccc9554f490ca1249619ced743ffca593ff5
Mike van Genderen <mikevg466>
2017-06-28 12:24:27 -0400

    refactor: Modularizes socket and app pieces on server side

---
+++ server/sockets/server.js
@@ -0,0 +1,77 @@
+const uuid = require('uuid');
+const serverReduxStore = require('../store');   // ****
+
+// server rooms is always empty
+// server.sockets.adapter.rooms has rooms but also has clientID's with no way to distinguish
+// client.rooms has rooms but also it's own id also with no real way to distinguish besides
+//      looking to see if roomId === client.id
+
+// example:
+// server.sockets.adapter.rooms = {
+//    123812903: 123812903,
+//    654654742: 654654742,
+//    457456345: 457456345,
+//    room-12412432: room-12412432,
+//    room-43534513: room-43534513
+// }
+
+// example:
+// client.rooms = {
+//    457456345: 457456345,
+//    room-12412432: room-12412432,
+//}
+
+const socketServer = {};
+socketServer.makeSocketServer = server => {
+  // create helper function to find all rooms on server adapter that starts with lobby-
+  //    this is used for every update call so helper is DRY
+  const findRoomsOnServer = () => {
+    let rooms = server.sockets.adapter.rooms ? Object.keys(server.sockets.adapter.rooms) : [];
+    rooms = rooms.filter(room => room.length > 5 && room.slice(0, 5) === 'room-');
+    return rooms
+  }
+
+  const findRoomForClient = client => {
+    const rooms = client.rooms ? Object.keys(client.rooms) : [];
+    const room = rooms.find(room => room.length > 5 && room.slice(0, 5) === 'room-');
+    return room;
+  }
+
+
+  server.on('connection', client => {
+    client.emit('update', findRoomsOnServer());
+    broadcastDebugMsg(client.id + ' has joined the server');
+
+    // disconnect updates room list for clients but does not need to leave
+    //  as empty rooms do not appear.  Still need to look into if this leaves
+    //   any leftover data when we create actual game state in rooms
+    client.on('disconnect', () => {
+      server.sockets.emit('update', findRoomsOnServer());
+      broadcastDebugMsg(client.id + ' has disconnected from the server');
+    });
+
+    // using join for both join and host
+    //   use default roomId so that if they are not joining an existing room
+    //    they are creating a new room
+    client.on('join', (roomID = 'room-' + uuid.v4()) => {
+      // join or create room
+      client.join(roomID);
+      server.sockets.emit('update', findRoomsOnServer());
+    });
+
+    // chatMessage uses new findRoomForClient helper method that find's the room
+    //   for the client that starts with "lobby-",  this removes the rooms that
+    //    are based on the clientId
+    client.on('chatMessage', msg => {
+      // find out which room the client is in
+      server.to(findRoomForClient(client)).emit('addChatMessage', msg, client.id);
+    });
+
+    function broadcastDebugMsg(msg) {
+      server.sockets.emit('debugMessage', msg);
+    }
+  });
+}
+
+
+module.exports = socketServer;
