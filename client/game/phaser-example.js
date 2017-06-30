68af42cb4f003674fb0e8fc847b94d81b2b0b4f9
Jasiu Leja <jleja09@gmail.com>
2017-06-29 21:57:43 -0400

    feature: adding robot hammer and animations

--- client/game/phaser-example.js
+++ client/game/phaser-example.js
@@ -1,27 +1,27 @@
-
-var game = new Phaser.Game(2000, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+const game = new Phaser.Game(2000, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.atlasJSONArray('smashbot', 'ourAssets/basic_movement_no_weapon.png', 'ourAssets/basic_movement_no_weapon.json');
     game.load.image('atari', 'assets/sprites/block.png');
     game.load.image('background', 'assets/games/starstruck/background2.png');
-    game.load.spritesheet('dude', 'ourAssets/basic_movement_no_weapon.png', 32, 48);
+    game.load.image('thorHammer', 'ourAssets/weapons/hammer_thors.png');
+    game.load.atlasJSONArray('smashbot', 'ourAssets/smashbot/basic_movement_no_weapon_final.png', 'ourAssets/smashbot/basic_movement_no_weapon_final.json');
+    game.load.physics('physicsData', 'ourAssets/smashbot/physics_data_smashbot.json');
 
 }
 
-var sprite;
-
-var player;
-var facing = 'left';
-var jumpTimer = 0;
-var cursors;
-var jumpButton;
-var yAxis = p2.vec2.fromValues(0, 1);
+let player,
+    thorHammer,
+    thorHammerPoly,
+    cursors,
+    attackButton,
+    facing = 'right',
+    jumpTimer = 0,
+    yAxis = p2.vec2.fromValues(0, 1);
 
 function create() {
 
-    var bg = game.add.tileSprite(0, 0, 2000, 600, 'background');
+    const bg = game.add.tileSprite(0, 0, 2000, 600, 'background');
 
     //  Enable p2 physics
     game.physics.startSystem(Phaser.Physics.P2JS);
@@ -30,29 +30,46 @@ function create() {
     game.physics.p2.world.defaultContactMaterial.friction = 0.3;
     game.physics.p2.world.setGlobalStiffness(1e5);
 
-    //  Add a sprite
+    //  Add a smashbot sprite and its animations
     player = game.add.sprite(200, 200, 'smashbot');
-    player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], 60, true);
-    player.animations.add('turn', [14], 20, true);
-    player.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], 60, true);
+    //  Enable physics. This creates a default rectangular body.
+    game.physics.p2.enable(player, true);
+    player.body.clearShapes();
+    player.body.loadPolygon('physicsData', 'smashbot000');
+    //player.scale.setTo(0.8);
+    player.anchor.setTo(0.5);
+    player.animations.add('right', [0, 1], 5, true); // arm out
+    player.animations.add('left', [14, 15], 5, true); // arm out
+    //player.animations.add('right', [7, 8, 9], 10, true); // arm in the air
+    //player.animations.add('left', [21, 22, 23], 10, true); // arm in the air
+    //player.animations.add('swingRight', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 60, true);
+    //player.animations.add('swingLeft', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], 60, true);
 
-    //  Enable if for physics. This creates a default rectangular body.
-    game.physics.p2.enable(player);
+    thorHammer = game.add.sprite(280, 203.5, 'thorHammer');
+    //thorHammer = player.addChild(game.make.sprite(280, 203.5, 'thorHammer'));
+    // player.addChild(thorHammer)
+    thorHammer.anchor.setTo(0, 1);
+    //thorHammer.alignIn(player, Phaser.CENTER, -58, -8);
+    thorHammer.alignTo(player, Phaser.TOP_RIGHT, 160, 0);
+    game.physics.p2.enable(thorHammer, true);
+    thorHammer.body.clearShapes();
+    thorHammer.body.loadPolygon('physicsData', 'hammer_thors_all');
+    thorHammer.body.static = true;
 
     player.body.fixedRotation = true;
     player.body.damping = 0.5;
 
-    var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial', player.body);
-    var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
-    var boxMaterial = game.physics.p2.createMaterial('worldMaterial');
+    const playerMaterial = game.physics.p2.createMaterial('playerMaterial', player.body);
+    //const weaponMaterial = game.physics.p2.createMaterial('weaponMaterial', thorHammer.body);
+    const worldMaterial = game.physics.p2.createMaterial('worldMaterial');
+    const boxMaterial = game.physics.p2.createMaterial('worldMaterial');
 
     //  4 trues = the 4 faces of the world in left, right, top, bottom order
     game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
 
     //  A stack of boxes - you'll stick to these
-    for (var i = 1; i < 4; i++)
-    {
-        var box = game.add.sprite(300, 645 - (95 * i), 'atari');
+    for (let i = 1; i < 4; i++) {
+        const box = game.add.sprite(300, 645 - (95 * i), 'atari');
         game.physics.p2.enable(box);
         box.body.mass = 6;
         // box.body.static = true;
@@ -61,73 +78,53 @@ function create() {
 
     //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
     //  those 2 materials collide it uses the following settings.
+    const groundPlayerCM = game.physics.p2.createContactMaterial(playerMaterial, worldMaterial, { friction: 0.0 });
+    //const groundWeaponCM = game.physics.p2.createContactMaterial(weaponMaterial, worldMaterial, { friction: 0.0 });
+    const groundBoxesCM = game.physics.p2.createContactMaterial(worldMaterial, boxMaterial, { friction: 0.6 });
+    //const playerWeaponCM = game.physics.p2.createContactMaterial(playerMaterial, weaponMaterial, { friction: 0.0 });
 
-    var groundPlayerCM = game.physics.p2.createContactMaterial(spriteMaterial, worldMaterial, { friction: 0.0 });
-    var groundBoxesCM = game.physics.p2.createContactMaterial(worldMaterial, boxMaterial, { friction: 0.6 });
-
-    //  Here are some more options you can set:
-
-    // contactMaterial.friction = 0.0;     // Friction to use in the contact of these two materials.
-    // contactMaterial.restitution = 0.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-    // contactMaterial.stiffness = 1e3;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-    // contactMaterial.relaxation = 0;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-    // contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-    // contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-    // contactMaterial.surfaceVelocity = 0.0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
-
-    var text = game.add.text(20, 20, 'move with arrow, space to jump', { fill: '#ffffff' });
+    const text = game.add.text(20, 20, 'move with arrow, space to jump', { fill: '#ffffff' });
 
     cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    attackButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 }
 
 function update() {
-
-    if (cursors.left.isDown)
-    {
+    //thorHammer.y = player.y;
+    if (cursors.left.isDown) {
         player.body.moveLeft(200);
+        thorHammer.body.moveLeft(200);
 
-        if (facing != 'left')
-        {
+
+        if (facing !== 'left') {
             player.animations.play('left');
             facing = 'left';
         }
-    }
-    else if (cursors.right.isDown)
-    {
+    } else if (cursors.right.isDown) {
         player.body.moveRight(200);
+        thorHammer.body.moveRight(200);
 
-        if (facing != 'right')
-        {
+        if (facing !== 'right') {
             player.animations.play('right');
             facing = 'right';
         }
-    }
-    else
-    {
+    } else {
         player.body.velocity.x = 0;
+        thorHammer.body.velocity.x = 0;
 
-        if (facing != 'idle')
-        {
+        if (facing !== 'idle') {
             player.animations.stop();
 
-            if (facing == 'left')
-            {
-                player.frame = 0;
-            }
-            else
-            {
-                player.frame = 5;
-            }
+            facing === 'left' ? player.frame = 14 : player.frame = 0;
 
             facing = 'idle';
         }
     }
 
-    if (jumpButton.isDown && game.time.now > jumpTimer && checkIfCanJump())
-    {
+    if (cursors.up.isDown && game.time.now > jumpTimer && checkIfCanJump()) {
         player.body.moveUp(300);
+        //thorHammer.body.moveUp(300);
         jumpTimer = game.time.now + 750;
     }
 
@@ -135,28 +132,32 @@ function update() {
 
 function checkIfCanJump() {
 
-    var result = false;
+    let result = false;
 
-    for (var i=0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++)
-    {
-        var c = game.physics.p2.world.narrowphase.contactEquations[i];
+    for (let i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+        let c = game.physics.p2.world.narrowphase.contactEquations[i];
 
-        if (c.bodyA === player.body.data || c.bodyB === player.body.data)
-        {
-            var d = p2.vec2.dot(c.normalA, yAxis);
+        if (c.bodyA === player.body.data || c.bodyB === player.body.data) {
+            let d = p2.vec2.dot(c.normalA, yAxis);
 
-            if (c.bodyA === player.body.data)
-            {
+            if (c.bodyA === player.body.data) {
                 d *= -1;
             }
 
-            if (d > 0.5)
-            {
+            if (d > 0.5) {
                 result = true;
             }
         }
     }
 
     return result;
+
+}
+
+    function render() {
+
+    // Sprite debug info
+    game.debug.spriteInfo(player, 232, 132);
+    //game.debug.spriteBounds(player);
 
 }
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,7 +2,7 @@ const db = require('../db');
 const User = require('./user');
 const Item = require('./item');
 
-User.hasMany(Item, { through: 'purchased_items' });
+User.belongsToMany(Item, { through: 'purchased_items' });
 User.hasOne(Item, { as: 'weapon' });
 User.hasOne(Item, { as: 'armor' });
 
