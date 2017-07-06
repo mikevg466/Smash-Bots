d8a3c3dc19816111b6ceebec101f9a64f74305fe
mikevg466 <mikevg466@gmail.com>
2017-07-06 17:01:58 -0400

    Update smashbotsGame.js

--- client/game/smashbotsGame.js
+++ client/game/smashbotsGame.js
@@ -3,7 +3,7 @@ import GameManager from './GameObjects/GameManager';
 import { updateLocalState, setWinner } from '../redux/game';
 import { emitPlayerStateChanges } from '../sockets/client';
 import store from '../store';
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 
 let hitBoxR,
   hitBoxL;
