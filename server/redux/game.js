62e58a7b3909b478ec543225695e4c01651c7bb4
Matthew Connelly <matthew.connelly9@gmail.com>
2017-07-06 11:11:01 -0400

    Update game.js

--- server/redux/game.js
+++ server/redux/game.js
@@ -27,7 +27,6 @@ gameRedux.reducers = (state = initialState, action) => {
 
     case ADD_PLAYERS:
       newState.players = action.players
-      console.log(action.players,'action.players')
       break;
 
     case UPDATE_PLAYERS:
