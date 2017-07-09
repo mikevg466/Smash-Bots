1ebacd0f29af5452a955b9e0d71db5e9fc167450
Jasiu Leja <jleja09@gmail.com>
2017-07-09 12:42:36 -0400

    Update Player.js

--- client/game/SpriteObjects/Player.js
+++ client/game/SpriteObjects/Player.js
@@ -7,8 +7,6 @@ export default class Player extends Sprite{
     this.playerNumber = playerNumber;
     this.game = game;
     
-    console.log(spriteName, "<=================")
-
     // ------ Animations -------
     this.setAnimation(
       'move',
