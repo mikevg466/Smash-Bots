3a00187bceb12bfaa4931796493438f9328e1a84
Jasiu Leja <jleja09@gmail.com>
2017-07-12 16:52:19 -0400

    fixes test: initial active players in game redux

--- test/redux/game.spec.js
+++ test/redux/game.spec.js
@@ -51,7 +51,7 @@ describe('Game Reducer', () => {
 
   it('has expected initial state', () => {
     expect(testStore.getState().game).to.be.deep.equal({
-      activePlayers: 3,
+      activePlayers: 4,
       isGamePlaying: false,
       playerNumber: 0,
       localPlayer: {},
